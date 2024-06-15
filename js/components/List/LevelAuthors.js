db738f4df01503d748127b1eb0384a5295ef6fa2
ExiledNeptunium <163695717+ExiledNeptunium@users.noreply.github.com>
2024-06-15 11:17:24 -0600

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -16,13 +16,13 @@ export default {
     template: `
         <div class="level-authors">
             <template v-if="selfVerified">
-                <div class="type-title-sm">Creator & Verifier</div>
+                <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
             </template>
             <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Creator</div>
+                <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
@@ -32,7 +32,7 @@ export default {
                 </p>
             </template>
             <template v-else>
-                <div class="type-title-sm">Creators</div>
+                <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
                     <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
                         <span >{{ creator }}</span
