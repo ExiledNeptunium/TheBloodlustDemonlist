d46c207e406a34b479dbc551d15f7dab0708a0cc
ExiledNeptunium <163695717+ExiledNeptunium@users.noreply.github.com>
2025-06-13 18:47:46 -0600

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,12 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Packs from './pages/Packs.js';
 
 export default [
     { path: '/', component: List },
+    { path: '/list/:level?', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/packs', component: Packs },
 ];
